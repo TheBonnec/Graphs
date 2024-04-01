a14fb9686becc655360e61f74ddbfd88aba6557f
Izura7 <94544223+Izura7@users.noreply.github.com>
2024-04-01 16:31:03 +0200

    class graph + class vertex

--- Model/Graph.py
+++ Model/Graph.py
@@ -1,3 +1,20 @@
+from Vertex import Vertex
+
 class Graph:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name : str, fileName : str) -> None:
+        self.name = name
+        fileCutted = 0
+        ListV = 0
+        file = open(fileName, "r")
+        for i in range(len(file)):
+            fileCutted[i] = file.readlines[i].split()
+        for i in range(len(fileCutted)):
+            ListV[i]=Vertex(fileCutted[i][0], fileCutted[i][1])
+
+        for i in range(len(ListV)):
+            for j in range(1, len(fileCutted[i])):
+                for k in range(len(ListV)):
+                    if (fileCutted[i][j] == ListV[k].value):
+                        ListV[i].addNextVertex(ListV[k])
+        self.ListV = ListV  
+        file.close()  

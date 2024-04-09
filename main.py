b75d2496da2c4da05418d237bacaaf1e134ce196
Izura7 <94544223+Izura7@users.noreply.github.com>
2024-04-09 14:32:17 +0200

    displayMenu fini

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from Model.Graph import Graph
 from View.displayGraph import displayGraph
+from View.displayMenu import displayMenu
 
 
 def main():
-    graph = Graph("graph1","GraphsFiles/table2.txt")
-    displayGraph(graph)
+    #graph = Graph("graph1","GraphsFiles/table2.txt")
+    #displayGraph(graph)
+    displayMenu()
     return
     
 

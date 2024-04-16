276f1bcadbd64adc8cfed93e4f71d571eed033e9
charlineblanc <charline.blanc@efrei.net>
2024-04-16 18:37:20 +0200

    deuxième commit

--- Model/Helper.py
+++ Model/Helper.py
@@ -1,7 +1,6 @@
-from asyncio import start_server
-from networkx import topological_sort
 from Model.Graph import Graph
 from Model.Vertex import Vertex
+import networkx as nx
 
 def has_negative_edges(graph: Graph) -> bool:
     for vertex in graph.listVertices:
@@ -10,3 +9,28 @@ def has_negative_edges(graph: Graph) -> bool:
                 return True  # Negative edge detected
     return False  # No negative edge found
 
+def calculate_latest_dates(graph: Graph):
+    G = nx.DiGraph()
+    for vertex in graph.listVertices:
+        for prev_vertex in vertex.previousVertices:
+            G.add_edge(prev_vertex.value, vertex.value, weight=int(vertex.duration))
+
+    # Vérifier s'il y a des cycles dans le graphe
+    if not nx.is_directed_acyclic_graph(G):
+        raise ValueError("Le graphe contient un cycle, impossible de calculer les dates les plus tardives.")
+    # --> tri topologique
+    topo_sorted_vertices = list(nx.topological_sort(G))
+    latest_start_times = {vertex: 0 for vertex in topo_sorted_vertices}
+
+    # Initialiser le dernier noeud
+    latest_start_times[topo_sorted_vertices[-1]] = int(graph.listVertices[-1].duration)
+
+    # Calculer les dates les plus tardives en remontant dans le tri topologique
+    for vertex in reversed(topo_sorted_vertices):
+        current_duration = int(next((v.duration for v in graph.listVertices if v.value == vertex), None))
+        for pred in G.predecessors(vertex):
+            latest_start_times[pred] = min(latest_start_times[pred], latest_start_times[vertex] - current_duration)
+    for vertex in topo_sorted_vertices:
+        print(f"Latest start time for task {vertex}: {latest_start_times[vertex]}")
+
+    return latest_start_times
--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from Model.Helper import has_negative_edges, calculate_latest_dates
 
 
 def main():
-    graph = Graph("graph1","GraphsFiles/table12.txt")
-    if has_negative_edges(graph):
-        print("Le graphe a des arêtes négatives.")
-    else:
-        print("Le graphe n'a pas d'arêtes négatives.")
-        
+    graph = Graph("graph1","GraphsFiles/table3.txt")
+    try:
+        if has_negative_edges(graph):
+            print("Le graphe a des arêtes négatives.")
+        else:
+            print("Le graphe n'a pas d'arêtes négatives.")
+            latest_dates = calculate_latest_dates(graph)   
+            print("Les dates les plus tardives ont été calculées.")
+    except ValueError as e:
+        print(e)    
         
 if __name__ == "__main__":
     main()
